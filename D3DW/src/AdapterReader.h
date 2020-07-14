6314f2a9014dd52c5bd0a2b475d2b7d971fb02b3
Zhang-Dev-byte <63158613+Zhang-Dev-byte@users.noreply.github.com>
2020-07-14 10:36:59 -0700

    Linker fixed

--- D3DW/src/AdapterReader.h
+++ D3DW/src/AdapterReader.h
@@ -1,7 +1,13 @@
 #pragma once
 #include "ErrorLogger.h"
 #include <d3d11.h>
-#pragma comment(lib,"d3d11.lib")
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
+#include <DirectXColors.h>
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "dxgi.lib")
+#pragma comment(lib, "d3dcompiler.lib")
+#pragma comment(lib, "winmm.lib")
 #pragma comment(lib,"DirectXTK.lib")
 #pragma comment(lib,"DXGI.lib")
 #include <wrl/client.h>
